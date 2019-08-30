337cf10a5c2a8bbb750a162f1e52aba8d175c32e
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2019-08-30 00:21:46 -0300

    Classe principal.
    Estrutura básica e TODOs.

---
+++ AlgoritmoEvolucionario.py
@@ -0,0 +1,22 @@
+'''
+Algoritmo Evolucionário - por Gabriel Cavalcante
+
+Classe responsável por tratar o problema com um algoritmo evolutivo.
+'''
+
+class Evo:
+    #
+    # TODO Criar métodos de manipulação de população.
+    evolucao():
+
+
+    # TODO Método para avaliação da população.
+
+    # TODO Método para selecionar pais.
+    
+    # TODO Método para recombinação e mutação da poppulação
+
+    # TODO Método para selecionar sobreviventes.
+
+    # TODO Criar método de inicialização da classe.
+
