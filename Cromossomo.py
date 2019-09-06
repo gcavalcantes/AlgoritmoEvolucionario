8383818786463c54f62b980fd3260e1275e54163
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2019-09-06 17:46:19 -0300

    Update cromossomo
    Mais funções básicas de um cromossomo.

--- Cromossomo.py
+++ Cromossomo.py
@@ -16,14 +16,33 @@ class Cromossomo():
         self.valor = ""
         self.avaliacao = -1
         
-    # TODO Método de atualização de valor do genes de um cromossomo
+    # Método de atualização de valor do genes de um cromossomo
     def alterar_valor(self, novo_valor):
         self.valor = novo_valor
         
-    # TODO Método de inicialização
+    # Método de inicialização da geração de números para o cromossomo
     def inicia(self):
+        # Variável para receber o novo gene
+        novo_valor = ""
+        # Para cada número no cromossomo
+        for i in range(self.tamanho):
+            # Se um número real for maior que 0,5
+            if random.random() > .5:
+                # Valor do gene é 1
+                novo_valor += '1'
+            else:
+                # Caso contrário o valor de gene é 0
+                novo_valor += '0'
+        self.alterar_valor(novo_valor)
+                
+    # TODO Método de crossover que recebe dois cromossomos
+    def crossover(self, aux_cromossomo):
+        # Divide o cromossomo aleatoriamente
+        corta_index = int(random.random() * self.tamanho)
+        novo_valor = ""
+        if random.random() > .5:
+            novo_valor = self.valor[0:corta_index] + aux_cromossomo
 
-    # TODO Método de crossover
 
     # TODO Método de mutação dos genes
 
