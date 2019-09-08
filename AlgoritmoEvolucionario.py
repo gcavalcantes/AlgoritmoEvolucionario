b85b44e9c8592bad629bc70ae46743307113f73a
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2019-09-08 14:33:44 -0300

    Algoritmo básico completo
    Todas as funções foram implementadas. Testes adicionais necessitam serem feitos.

--- AlgoritmoEvolucionario.py
+++ AlgoritmoEvolucionario.py
@@ -3,20 +3,75 @@ Algoritmo Evolucionário - por Gabriel Cavalcante
 
 Classe responsável por tratar o problema com um algoritmo evolutivo.
 '''
+from Cromossomo import Cromossomo
+import random, time
 
-class Evo:
-    #
-    # TODO Criar métodos de manipulação de população.
-    evolucao():
+class Evo():
+    # Método __init__
+    def __init__(self, tam_pop = 20, geracoes = 1000):
+        
+        self.pop = []
+        self.tam_populacao = tam_pop
+        self.geracoes = geracoes
+        self.soma_avaliacoes = 0
 
+    # Criar métodos de manipulação de população.
+    def popular(self):
+        for i in range(self.tam_populacao):
+            self.pop.append(Cromossomo(10))
+        for cromossomo in self.pop: 
+            cromossomo.inicia()
 
-    # TODO Método para avaliação da população.
+    # Método para avaliação da população.
+    def avalia(self):
+        self.soma_avaliacoes = 0
+        for Cromossomo in self.pop:
+            self.soma_avaliacoes += Cromossomo.avalia()
 
-    # TODO Método para selecionar pais.
+    # Método de seleção entre os indivíduos avaliados.
+    def escolha(self):
+        self.avalia()
+        max = random.random() * self.soma_avaliacoes
+        i, aux = [0, 0]
+        random.shuffle(self.pop)
+        while aux < max and i < self.tam_populacao:
+            aux += self.pop[i].avalia
+            i += 1
+        i -= 1
+        return i
     
-    # TODO Método para recombinação e mutação da poppulação
+    # Método para selecionar pais.
+    def nova_g(self):
+        nova_pop = []
+        for i in range(self.tam_populacao):
+            pai1 = self.pop[self.escolha()]
+            pai2 = self.pop[self.escolha()]
+            filho = pai1.crossover(pai2)
+            filho.mutacao(.05)
+            nova_pop.append(filho)
+        return nova_pop
+    
+    # TODO Método de inicialização da classe.
+    def algo_genetico(self):
+        print("====================\nSimulação de mutação genético\n====================")
+        self.popular()
+        self.avalia()
+        ordem = sorted(self.pop, key = lambda x: x.avalia, reverse = True)
+        for i in range(self.geracoes):
+            top1 = ordem[0]
+            top2 = ordem[1]
+            top3 = ordem[2]
+            top4 = ordem[3]
+            top4 = ordem[4]
+            top5 = ordem[4]
+            print("Cinco melhroes da geração %d:\n%s\n%s\n%s\n%s\n%s\n\n" % (i, top1, top2, top3, top4, top5), end="", flush = True)
+            time.sleep(.5)
 
-    # TODO Método para selecionar sobreviventes.
+            self.pop = self.nova_g()
+            self.avalia()
+            ordem = sorted(self.pop, key = lambda x: x.avalia, reverse = True)
+        print('Melhor avaliado -> %s' % (ordem[1]))
 
-    # TODO Criar método de inicialização da classe.
-
+# Nova instância para executar algoritmo
+evolucao = Evo(tam_pop = 40, geracoes = 5)
+evolucao.algo_genetico()
--- Cromossomo.py
+++ Cromossomo.py
@@ -35,19 +35,49 @@ class Cromossomo():
                 novo_valor += '0'
         self.alterar_valor(novo_valor)
                 
-    # TODO Método de crossover que recebe dois cromossomos
+    # Método de crossover que recebe dois cromossomos
     def crossover(self, aux_cromossomo):
         # Divide o cromossomo aleatoriamente
         corta_index = int(random.random() * self.tamanho)
         novo_valor = ""
         if random.random() > .5:
-            novo_valor = self.valor[0:corta_index] + aux_cromossomo
+            novo_valor = self.valor[0:corta_index] + aux_cromossomo.valor[corta_index:len(aux_cromossomo.valor)]
+        else:
+            novo_valor = aux_cromossomo.valor[0:corta_index] + self.valor[corta_index:len(aux_cromossomo.valor)]
+        cromossomo_filho = Cromossomo(novo_valor)
+        cromossomo_filho.alterar_valor(novo_valor)
+        return cromossomo_filho
+    
+    # Método de mutação dos genes
+    def mutacao(self,perc_mutacao):
+        comeco, aux, fim = ['','','']
+        # Para cada caractere em um cromossomo
+        for i in range(self.tamanho):
+            # Se um número aleatório for menor que o percentual de mutação
+            if random.random() < perc_mutacao:
+                # A cada iteração as variáveis recebem uma troca de valor de acordo com 'i', ou não
+                comeco = self.valor[0:i]
+                fim = self.valor[i+1:self.tamanho]
+                aux = self.valor[i]
+                # Se a variável aux for igual a 1
+                if aux == '1':
+                    aux = '0'
+                else:
+                    aux ='1'
+                # Variável valor recebe a nova mistura representando a mutação.
+                self.alterar_valor(comeco+aux+fim)
 
+    # Método para valores reais
+    def v_real(self, i = 0, s = 100):
+        return i + (s - i)/(2 ** self.tamanho -1) * int(self.valor, 2)
 
-    # TODO Método de mutação dos genes
+    # Método para avaliar cromossomo
+    def avaliar(self):
+        x = int(self.valor, 2)
+        # Fórmula de avaliação seno(x²)/(3 - cos(e) - x)
+        self.avaliacao = math.sin(x**2)/(3-math.cos(math.e) - x)
+        return self.avaliacao
 
-    # TODO Método para valores reais
-
-    # TODO Método para avaliar cromossomo
-
-    # TODO Método para representar o indivíduo como String
+    # Método para representar o indivíduo como String
+    def __repr__(self):
+        return "Cromossomo:[%s] | Avaliacao[%.2f] | Valor[%d]" % (self.valor, self.avaliacao, int(self.valor, 2))
