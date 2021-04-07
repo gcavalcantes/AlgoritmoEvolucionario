1a28f28486265ad2afa2e1984b6ca76230bc9660
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2021-04-06 22:22:44 -0300

    Endentação e arquivos
    Remoção dos arquivos temporários.
    Endentação e erros do arquivos principal foram resolvidos. Próximo passo é terminar as funções.

--- principal.py
+++ principal.py
@@ -2,53 +2,53 @@
 Arquivo responsável por reunir todos os outros para o algoritmo genético.
 '''
 
+import ProblemaMochila
+import Itens
+
 
 class Principal():
     '''
     Classe principal do programa.
     '''
-
-
     # Variável para o número de itens na solução
-int pergunta1
-# Variável para o valor de cada item na solução
-int pergunta2
+    pergunta1 = 0
+    # Variável para o valor de cada item na solução
+    valorItem = 0
 
-# TODO Função para fazer as perguntas.
+    # TODO Função para fazer as perguntas.
+    def perguntas():
+        """
+        """
+        print("Algoritmo genético para o problema da mochila. /n Desenvolvido por Gabriel Cavalcante.")
+        pergunta_1()
+        pergunta_2()
+        pergunta3 = input(print(""))
 
+    # TODO Função para a pergunta 1
+    def pergunta_1():
+        """
+        Função que retorna o número de itens que será usado na solução e também pelos outros métodos da classe.
+        """
 
-def perguntas():
-		"""
-		"""
-		print("Algoritmo genético para o problema da mochila. /nDesenvolvido por Gabriel Cavalcante.")
-		pergunta_1()
-		pergunta_2()
-		String pergunta3 = input(print("")
-
-                           # Função para a pergunta 1
-                           def pergunta_1():
-                           """
-		Função que retorna o número de itens que será usado na solução e também pelos outros médotos da classe.
-		"""
-        int pergunta1
+        pergunta1
         try:
-        pergunta1=val(input(print("Quantos itens terá a lista? (Número inteiro)"))
-                        while (pergunta1 < 2):
-                        print("Valor deve ser maior que um")
-                        pergunta1=val(input(print("Quantos itens terá a lista? (Número inteiro)"))
-                                                       except ValueError:
-                                                       print(
-                                             'ERRO: Resposta precisa ser um número inteiro.')
+            pergunta1 = input(
+                print("Quantos itens terá a lista? (Número inteiro)"))
+            while (pergunta1 < 2):
+                print("Valor dever ser maior que um.")
+                pergunta1 = input(
+                    print("Quantos itens terá a lista? (Número inteiro)"))
+        except ValueError:
+            print('Erro: Resposta precisa ser um número inteiro.')
 
-                               return pergunta1
+        return pergunta1
 
-                               # TODO Função para a pergunta 2
-                               def pergunta_2(int numItens):
-                               """
-		"""
-                               int pergunta2
-                               int contador=1
-                               while contador <= numItens:
-                               print("Qual o valor do item {}".format(numItens))
-                               pergunta2=input(
-                                             print("Qual o valor do item?"))
+    # TODO Função para a pergunta 2
+    def pergunta_2(self, numItens):
+        """
+        """
+        pergunta2
+        contador = 1
+        while contador <= numItens:
+            print('Qual o valor do intem {}'.format(numItens))
+            pergunta2 = input(print('Qual o valor do item?'))
