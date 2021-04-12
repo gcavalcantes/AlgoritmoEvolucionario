fb89a5e305963c09ff174e230f33fb191a99eab0
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2021-04-11 22:59:21 -0300

    Perguntas
    Adicionadas nova pergunta para definir o peso do item na mochila.
    Adicionado o template do método para perguntar o tamanho da mochila.

--- principal.py
+++ principal.py
@@ -12,32 +12,33 @@ class Principal():
     '''
     # Variável para o número de itens na solução
     pergunta1 = 0
-    # Variável para o valor de cada item na solução
-    valorItem = 0
+    # Variável para o valor do item na pergunta 2. Deve ser int.
+    pergunta2Valor = 0
+    # Variável para o peso do item na pergunta 2. Deve ser float.
+    pergunta2Peso = 0.0
 
     # TODO Função para fazer as perguntas.
-    def perguntas():
+    def perguntas(self):
         """
         """
         print("Algoritmo genético para o problema da mochila. /n Desenvolvido por Gabriel Cavalcante.")
-        pergunta_1()
-        pergunta_2()
-        pergunta3 = input(print(""))
+        numItens = pergunta_1()
+        pergunta_2(numItens)
+        pergunta_3()
 
     # TODO Função para a pergunta 1
-    def pergunta_1():
+    def pergunta_1(self):
         """
         Função que retorna o número de itens que será usado na solução e também pelos outros métodos da classe.
         """
 
-        pergunta1
         try:
-            pergunta1 = input(
-                print("Quantos itens terá a lista? (Número inteiro)"))
+            pergunta1 = int(input(
+                print("Quantos itens terá a lista? (Número inteiro)")))
             while (pergunta1 < 2):
                 print("Valor dever ser maior que um.")
                 pergunta1 = int(input(
-                    print("Quantos itens terá a lista? (Número inteiro)")))
+                    print("Quantos itens terá a lista? (Número inteiro) ")))
         except ValueError:
             print('Erro: Resposta precisa ser um número inteiro.')
 
@@ -47,13 +48,22 @@ class Principal():
     def pergunta_2(self, numItens):
         """
         """
-        pergunta2
+
         contador = 1
         try:
+            # Loop para a segunda pergunta.
             while contador <= numItens:
-                print('Qual o valor do intem {}'.format(numItens))
-                pergunta2 = input(
-                    print('Qual o valor do item {}?').format(contador))
+                pergunta2Valor = int(input(
+                    print('Qual o valor do item {}?').format(contador)))
+                pergunta2Peso = float(
+                    input(print('Qual o peso do item {}?').format(contador)))
                 contador += 1
-        except ValueError:
+        # Tratamento de erro para caso a pergunta não seja um inteiro.
+        except ValueError as ve:
             print('Erro: Resposta precisa ser um número inteiro.')
+
+    # TODO Função para a pergunta 3
+    def pergunta_3(self):
+        """
+        Função responsável por definir o tamanho da mochila
+        """
