64898ebed40db17b19d8d21f737269b8b376c067
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2021-05-05 08:59:57 -0300

    Reestrutura e Fuzzy
    Começo da implementação do código da lógica fuzzy.

---
+++ Fuzzy/Fuzzy.py
@@ -0,0 +1,13 @@
+'''
+'''
+
+
+class Fuzzy():
+    '''
+    '''
+
+    def __init__(self):
+        '''
+        Construto de OOP que cria um objeto quando a classe é chamada
+                e é passado como o primeiro parâmetro deste método.
+        '''
---
+++ Fuzzy/Principal.py
@@ -0,0 +1,11 @@
+'''
+'''
+
+class Principal():
+    '''
+    '''
+    def __init__(self):
+        '''
+        Construto de OOP que cria um objeto quando a classe é chamada
+                e é passado como o primeiro parâmetro deste método.
+        '''
